89279a760c2cd3bfbf94c2bc1c809da907c47863
hayaa1234 <hayalawansah@gmail.com>
2021-12-06 21:08:15 +0200

    finished lab 27

--- snacks_project/snacks/admin.py
+++ snacks_project/snacks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.admin.decorators import register
+from .models import Snack
 # Register your models here.
+
+@admin.register(Snack)
+class AdminClass(admin.ModelAdmin):
+    list_display  = ['name',"purchaser","description"]
+    search_fields = ["name"]
+# admin.site.register(Snack)
+
+
+
--- snacks_project/snacks/tests.py
+++ snacks_project/snacks/tests.py
@@ -1,9 +1,9 @@
 from django.http import response
 from django.test import TestCase
-from django.test.testcases import SimpleTestCase
+from django.test.testcases import SimpleTestCase,TestCase
 from django.urls import reverse
 # Create your tests here.
-class SnacksTests(SimpleTestCase):
+class SnacksTests(TestCase):
     def test_home_status_code(self):
         url = reverse("home")
         response = self.client.get(url)
@@ -13,6 +13,16 @@ class SnacksTests(SimpleTestCase):
         url = reverse("about-us")
         response = self.client.get(url)
         self.assertEqual(response.status_code,200)
+
+    def test_snack_list_status_code(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    # def test_snack_detail_status_code(self):
+    #     url = reverse("snack_detail")
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code,200)
 
     def test_home_page_template(self):
         url = reverse("home")
@@ -25,5 +35,17 @@ class SnacksTests(SimpleTestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response,"About_us.html")
         self.assertTemplateUsed(response,"_base.html")  
+
+    def test_snack_list_page_template(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,"snack_list.html")
+        self.assertTemplateUsed(response,"_base.html") 
+
+    # def test_snack_detail_page_template(self):
+    #     url = reverse("snack_detail")
+    #     response = self.client.get(url)
+    #     self.assertTemplateUsed(response,"snack_detail.html")
+    #     self.assertTemplateUsed(response,"_base.html")  
 
     
